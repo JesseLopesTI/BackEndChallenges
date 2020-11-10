d236fabd356630c1a62f928e68352619501661de
JesseLopesTI <jesselopes.ti2020@gmail.com>
2020-11-10 20:54:05 -0300

    Definindo o escopo da classe ImprimirImposto.java

--- challenge-001/imposto/src/com/br/imposto/InformarImposto.java
+++ challenge-001/imposto/src/com/br/imposto/InformarImposto.java
@@ -2,4 +2,14 @@ package com.br.imposto;
 
 public class InformarImposto {
 
+	public static void main(String[] args) {
+
+		CalcularImposto informar = new CalcularImposto();
+
+		informar.informarDados();
+		informar.calcularDependentes();
+		informar.calcularInss();
+		informar.calcularIrpf();
+		informar.imprimirResultado();
+	}
 }
